b8aa0b9bbe008ce27c7b6bfaaf22bbb4f299b5f8
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-03-31 17:57:09 -0500

    Uso de Get and set en POO

---
+++ src/Mamifero.java
@@ -0,0 +1,47 @@
+public class Mamifero {
+    public String name;
+    public String color;
+    public String tipo;
+    public int tamanio;
+
+    private int edadMaximo;
+
+    public double peso;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String _name){
+        this.name= _name;
+    }
+    public String getColor(){
+        return color;
+    }
+    public void setColor(String _color){
+        this.color = _color;
+    }
+    public String getTipo(){
+        return tipo;
+    }
+    public void setTipo(String _tipo){
+        this.tipo= _tipo;
+    }
+    public int getTamanio(){
+        return tamanio;
+    }
+    public void setTamanio(int _tamanio){
+        this.tamanio= _tamanio;
+    }
+    public int getEdadMaximo(){
+        return edadMaximo;
+    }
+    public void setEdadMaximo(int _edadMaximo){
+        this.edadMaximo= _edadMaximo;
+    }
+    public double getPeso(){
+        return peso;
+    }
+    public void setPeso(double _peso){
+        this.peso = _peso;
+    }
+}
